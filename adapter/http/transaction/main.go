e311614effc4776e1aa1c5f941317dd23f8c1d4b
Marco Paulo Ollivier <marcopollivier@gmail.com>
2020-04-22 03:04:46 -0300

    Coloca em memória

--- adapter/http/transaction/main.go
+++ adapter/http/transaction/main.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/marcopollivier/dio-expert-session-pre-class/model/transaction"
-	"github.com/marcopollivier/dio-expert-session-pre-class/util"
 	"io/ioutil"
 	"net/http"
 )
+
+var transactions = transaction.Transactions{}
 
 func GetTransactions(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
@@ -17,29 +18,6 @@ func GetTransactions(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-type", "application/json")
 
-	var transactions = transaction.Transactions{
-		transaction.Transaction{
-			Title:       "Salário",
-			Amount:      1200.0,
-			Type:        0,
-			Installment: 1,
-			CreatedAt:   util.StringToTime("2020-04-05T11:45:26"),
-		},
-		transaction.Transaction{
-			Title:     "Conta de luz",
-			Amount:    100.0,
-			Type:      1,
-			Installment: 1,
-			CreatedAt: util.StringToTime("2020-04-12T22:00:00"),
-		},
-		transaction.Transaction{
-			Title:     "Compra telefone celular",
-			Amount:    999.99,
-			Type:      1,
-			Installment: 10,
-			CreatedAt: util.StringToTime("2020-04-20T11:00:26"),
-		},
-	}
 	_ = json.NewEncoder(w).Encode(transactions)
 }
 
@@ -49,9 +27,8 @@ func CreateATransaction(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var res = transaction.Transactions{}
 	var body, _ = ioutil.ReadAll(r.Body)
-	_ = json.Unmarshal(body, &res)
+	_ = json.Unmarshal(body, &transactions)
 
-	fmt.Println(res)
+	fmt.Println(transactions)
 }
